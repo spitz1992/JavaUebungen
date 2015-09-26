2123274023cae517f0ea3e53671c376f327ad059
Johannes Wolf <Johannes@Johanness-MBP.fritz.box>
2015-09-26 16:55:36 +0200

    Übung

---
+++ zellerscheformel/src/zellerscheformel/array.java
@@ -0,0 +1,23 @@
+package zellerscheformel;
+
+public class array {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        int [] iaMayArray = {2,3,2,5,3,2};
+        
+        for (int i = 0; i < iaMayArray.length;i++)
+        {
+            String strDarstellung = "";
+            
+            for (int n = 0; n < iaMayArray[i]; ++n) {
+                strDarstellung = strDarstellung + "x";
+                
+            }
+            System.out.println(i+ " = " + strDarstellung);
+        }
+            
+        
+    }
+}
+ 
---
+++ zellerscheformel/src/zellerscheformel/zellersche.java
@@ -0,0 +1,77 @@
+package zellerscheformel;
+
+import javax.swing.JOptionPane;
+public class zellersche {
+
+	public static void main(String[] args) {
+		String sEingabeUserTag ="";
+		String sEingabeUserJahr ="";
+		String sEingabeUserMonat ="";
+		int iHundert =0;
+		int iJahreszahl = 0;
+		int iMonat = 0;
+		int iTag = 0;
+		
+		
+		sEingabeUserTag = JOptionPane.showInputDialog ("Eingabe Datum Tag");
+		sEingabeUserMonat = JOptionPane.showInputDialog ("Eingabe Datum Monat");
+		sEingabeUserJahr = JOptionPane.showInputDialog ("Eingabe Datum Jahr");
+		iJahreszahl = Integer.parseInt(sEingabeUserJahr);
+		iMonat = Integer.parseInt(sEingabeUserMonat);
+		iTag = Integer.parseInt(sEingabeUserTag);
+		
+		System.out.print(sEingabeUserTag +"-"+sEingabeUserMonat +"-" + sEingabeUserJahr + "\t");
+		iHundert = iJahreszahl / 100;
+		iJahreszahl = iJahreszahl % 100;
+		
+		if(iMonat <3){
+			iMonat = iMonat + 10;
+			if(iJahreszahl == 0){
+				iJahreszahl = 99;
+				iHundert = iHundert - 1;
+			}else{	
+				iJahreszahl = iJahreszahl - 1;
+			}
+		}else{
+			iMonat = iMonat -2;
+		}
+		int iWochentag = (iTag + (int)(2.61*iMonat-0.2)+iJahreszahl + (int)(iJahreszahl/4) + (int)(iHundert/4)-2*iHundert);
+		if(iWochentag % 7 >= 0){
+			iWochentag = iWochentag % 7;
+		} else {
+			iWochentag = 7 - Math.abs(iWochentag % 7);
+		}
+		switch (iWochentag)
+		{
+		case 0:
+			System.out.println("Sonntag");
+			break;
+			
+		case 1:
+			System.out.println("Montag");
+			break;
+			
+		case 2:
+			System.out.println("Dienstag");
+			break;
+			
+		case 3:
+			System.out.println("Mittwoch");
+			break;
+			
+		case 4:
+			System.out.println("Donnerstag");
+			break;
+			
+		case 5:
+			System.out.println("Freitag");
+			break;
+			
+		case 6:
+			System.out.println("Samstag");
+			break;
+		}
+		
+	}
+
+}
