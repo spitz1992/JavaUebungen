4dd2f396d5e0f64a215e7ccab94fe45f57cf0cb9
spitz1992 <johanneswolf1@gmx.de>
2015-10-07 15:17:31 +0200

    Namen geändert

---
+++ zellerscheformel/src/zellerscheformel/ArrayTestSauelen.java
@@ -0,0 +1,25 @@
+package zellerscheformel;
+
+public class ArrayTestSauelen 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int [] iMayArray = {2,3,2,5,3,2};
+		int iZaehler = 0;
+			
+			for (iZaehler = 0; iZaehler < iMayArray.length; iZaehler ++) 
+			{
+				System.out.println(iZaehler);
+			}
+				for (int z = 0; z < iZaehler; z++) 
+				{
+					System.out.println(z);
+				}
+				
+	
+	}
+
+
+}
